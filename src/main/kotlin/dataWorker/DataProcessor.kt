d03e19076617870489e4dc3dc13002d429545674
Iván Azagra <ivanazagratroya@gmail.com>
2022-10-19 22:46:46 +0200

    Html con atributos

--- src/main/kotlin/dataWorker/DataProcessor.kt
+++ src/main/kotlin/dataWorker/DataProcessor.kt
@@ -145,4 +145,121 @@ class DataProcessor(val contenedorData: List<Contenedor>, val residuoData: List<
         ggsave(gMaxMinMeanStd, "Toneladas-Totales-Tipo-Residuo.png", 1, 2, imagesPath.toString())
     }
 
+    fun generateSummary(): String {
+        return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="style.css">
+                <title>Práctica Acceso a Datos 01</title>
+            </head>
+            <body>
+                <!--Archivo resumen Madrid"-->
+                <h1 align="center" >Historial de recogida de basura y reciclaje de Madrid </h1>
+
+                <br>
+                <div id="contenedor">
+                    <h4>Contenedores por tipo en cada distrito: </h4>
+                    <p>$contenedoresDistrito</p>
+
+                    <p>
+                        <h4>Contenedores totales por distrito</h4>
+                    <p> 
+                        <img src= "./graphics/Contenedores-Por-Distrito.png"/>
+                    </p>
+
+                    </p>
+
+                    <h4>Media de las toneladas anuales de recogidas por cada tipo de basura agrupado por distrito: ${executionDTO.mediaToneladasAnuales}</h4>
+
+                    <p>
+                        <h4>Media de las toneladas mensuales de recogida por distrito</h4>
+                        <p> 
+                        <img src= "./graphics/Media-Toneladas-Mensuales-Distrito.png"/>
+                    </p>
+
+                    </p>
+
+                    <p align="left">
+                        <h4>Máximo, mínimo, media y desviación de toneladas anuales de recogida por cada tipo de basura agrupadas por distrito</h5>
+                            <p>$MaxMinMeanStd</p>
+                        </h4>
+                    </p>
+                    <h4>Suma de todo lo recogido en un año por distrito $sumaRecogidoDistrito </h4>
+                    <h4>Cantidad de cada tipo de residuo recogida por distrito</h4>
+                    <p align="right">Tiempo de generación del mismo en milisegundos: <i>${executionDTO.tiempoGeneracion}</i></p>
+                </div>
+                <!-- los id son para definirlos en la misma línea del html generado a través del css3-->
+                <div id="nombres">
+                    <strong>Iván Azagra y Daniel Rodríguez</strong>
+                </div>
+                <div id="fecha">
+                    ${executionDTO.fechaGeneracion}
+                </div>
+            </body>
+            </html>
+        """.trimIndent()
+    }
+
+    fun generateDistrictSummary(): String {
+        return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="style.css">
+                <title>Práctica Acceso a Datos 01</title>
+            </head>
+            <body>
+                <!--Archivo "Resumen_distrito"-->
+                <h1 align="center" >Historial de recogida de basura y reciclaje</h1>
+                <br>
+                <div id="contenedor">
+                    <h5>Contenedores por tipo: </h5>
+                    <p>$contenedoresDistrito</p>
+
+                    <br>
+                    <h5>Toneladas de residuo totales recogidas</h5>
+                    <p>(inserte $sumaRecogidoDistrito)</p>
+
+                    <p>
+                        <h4>Toneladas totales de residuo recogidas</h4>
+                        <p>
+                            <img src= "./graphics/Toneladas-Totales-Tipo-Residuo.png"/>
+                        </p>
+
+                    </p>
+
+                    <p align="left">
+                        <h4>Máximo, mínimo, media y desviación por mes por residuo en $MaxMinMeanStd</h4>
+                        
+                        
+                
+                        <h4>(_gráfico_)Máximo, mínimo y media por meses</h4>
+                        <p>
+                            <img src= "./graphics/Max-Min-Media-Desviacion-Distrito.png"/>
+                        </p>
+                    </p>
+                    <p align="right">Tiempo de generación del mismo en milisegundos: <i>${executionDTO.tiempoGeneracion}</i></p>
+                </div>
+                <br>
+                <!-- los id son para definirlos en la misma línea del html generado a través del css3-->
+                <div id="nombres">
+                    <strong>Iván Azagra y Daniel Rodríguez</strong>
+                </div>
+                <div id="fecha">
+                    ${executionDTO.fechaGeneracion}
+                </div>
+                
+                
+            </body>
+            </html>
+        """.trimIndent()
+    }
+
 }
